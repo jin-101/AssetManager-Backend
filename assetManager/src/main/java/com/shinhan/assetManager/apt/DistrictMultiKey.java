2594a1d12afd1194cb1cef8e296aab16cc49e449
djdjdddd <djdj2297@gmail.com>
2023-05-31 18:09:15 +0900

    5.31 용희, district 테이블 복합키 설정하였음

---
+++ assetManager/src/main/java/com/shinhan/assetManager/apt/DistrictMultiKey.java
@@ -0,0 +1,18 @@
+package com.shinhan.assetManager.apt;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class DistrictMultiKey implements Serializable {
+
+	private String dong; // 동 (ex. 제기동)
+	private String gu; // 구 (ex. 11110)
+}
