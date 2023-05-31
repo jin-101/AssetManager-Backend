b83bd56a59442d78064fca48793cf4f8207c4686
djdjdddd <djdj2297@gmail.com>
2023-05-31 12:43:09 +0900

    새 프로젝트 생성

---
+++ assetManager/src/main/java/com/shinhan/assetManager/AssetManagerApplication.java
@@ -0,0 +1,13 @@
+package com.shinhan.assetManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetManagerApplication.class, args);
+	}
+
+}
