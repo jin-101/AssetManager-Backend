7b7d32042f8917261224588c1f173168d541064a
fun7601 <fun7601@naver.com>
2023-06-07 09:28:21 +0900

    feat 카드거래 내역 불러오기

---
+++ assetManager/src/main/java/com/shinhan/assetManager/repository/HouseholdAccountsCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.shinhan.assetManager.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.shinhan.assetManager.dto.HouseholdAccountsCategoryDTO;
+
+public interface HouseholdAccountsCategoryRepository extends CrudRepository<HouseholdAccountsCategoryDTO, Integer>{
+
+}
