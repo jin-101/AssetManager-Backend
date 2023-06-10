dac634dcdc16b096d3274b2ea1c3f99794a7b01e
Han Jin <gkswls4194@gmail.com>
2023-06-10 18:46:34 +0900

    fix: deposit retrun code 수정

--- assetManager/src/main/java/com/shinhan/assetManager/controller/DepositController.java
+++ assetManager/src/main/java/com/shinhan/assetManager/controller/DepositController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shinhan.assetManager.deposit.DepositDtoForReact;
@@ -15,13 +16,14 @@ import com.shinhan.assetManager.deposit.DepositDtoForReact;
 public class DepositController {
 		//예적금 추가 버튼 클릭시 => 
 		@PostMapping(value = "/add.do")
+		@ResponseBody
 		public String addDeposit(@RequestBody DepositDtoForReact[] depositList) {
 			String result = "";
 			for(DepositDtoForReact de : depositList) {
 				System.out.println(de.toString());
 				//db에 넣는 로직
 			}
-			result = "성공";
+			result = "예/적금 추가에 성공하였습니다.";
 			return result;
 		}
 		
