b3474c6e39639c0c2e1a22c3c50a8551cd493925
qoehd4 <qoehd4@gmail.com>
2023-06-15 17:49:52 +0900

    주식 평균단가 완료

--- assetManager/src/main/java/com/shinhan/assetManager/dto/ExcelDTO.java
+++ assetManager/src/main/java/com/shinhan/assetManager/dto/ExcelDTO.java
@@ -1,6 +1,5 @@
 package com.shinhan.assetManager.dto;
 
-import java.io.Serializable;
 import java.sql.Date;
 
 import lombok.Getter;
@@ -11,18 +10,13 @@ import lombok.ToString;
 
 @Getter@Setter@NoArgsConstructor@ToString
 public class ExcelDTO {
-	String detail_code;
-	String 회원ID;
 	String 계좌번호;
-	String 은행;
 	Date 거래일자;
 	String 거래시간;
 	Integer 출금;
 	Integer 입금;
 	String 내용;
 	Integer 잔액;
-	HouseholdAccountsCategoryDTO category;
-	String memo;
 	
 }
 
