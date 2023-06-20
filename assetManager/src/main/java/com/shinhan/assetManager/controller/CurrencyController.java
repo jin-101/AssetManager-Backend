61799923d09835a3348a678171dc18d2055852e0
qoehd4 <qoehd4@gmail.com>
2023-06-20 17:24:27 +0900

    그므그믁ㅁ

--- assetManager/src/main/java/com/shinhan/assetManager/controller/CurrencyController.java
+++ assetManager/src/main/java/com/shinhan/assetManager/controller/CurrencyController.java
@@ -1,49 +1,37 @@
 package com.shinhan.assetManager.controller;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.shinhan.assetManager.mapping.StockTableEntityMapping;
 import com.shinhan.assetManager.realAssets.CurrencyInputDTO;
-import com.shinhan.assetManager.repository.UserAssetRepo;
-import com.shinhan.assetManager.repository.UserRepo;
-import com.shinhan.assetManager.user.UserAssetDTO;
-import com.shinhan.assetManager.user.UserDTO;
+import com.shinhan.assetManager.service.CurrencyService;
 
 @Controller
 @RequestMapping("/currency")
 public class CurrencyController {
-	//test
-	private String assetCode = "A2";
 	
 	@Autowired
-	private UserRepo userRepo;
-	
-	@Autowired
-	private UserAssetRepo userAssetRepo;
+	private CurrencyService currencyService;
 	
 	@PostMapping("/currencyAssetInput")
 	@ResponseBody
 	public String handleCurrencyInputRequest(CurrencyInputDTO currencyInputDTO) {
-		String response = "등록완료";	
+		String response = currencyService.registerCurrency(currencyInputDTO);
 		
-		try {
-			
-			System.out.println(currencyInputDTO);
-			Optional<UserDTO> user = userRepo.findById(currencyInputDTO.getUserId());
-			UserAssetDTO userAssetDto = new UserAssetDTO(user.get(), assetCode, 
-					currencyInputDTO.getCurrency(), currencyInputDTO.getPrice(), 
-					currencyInputDTO.getBuyDay(), currencyInputDTO.getShares());
-			
-			userAssetRepo.save(userAssetDto);
-		} catch (Exception e) {
-			response = "등록실패";
-		}
+
+		return response;
+	}
+	
+	@GetMapping("/currencyCrud")
+	@ResponseBody
+	public String handleShowHoldingCurrencyRequest(@RequestParam String id) {
+		
+		String response = currencyService.showHoldingCurrency(id);
 		
 		return response;
 	}
