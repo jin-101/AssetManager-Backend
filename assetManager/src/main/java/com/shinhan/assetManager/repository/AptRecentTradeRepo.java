4934015c6eedb694360d42bec882715429f811aa
djdjdddd <djdj2297@gmail.com>
2023-06-21 15:24:57 +0900

    docs: 최근거래내역 찾기 메소드 추가

--- assetManager/src/main/java/com/shinhan/assetManager/repository/AptRecentTradeRepo.java
+++ assetManager/src/main/java/com/shinhan/assetManager/repository/AptRecentTradeRepo.java
@@ -8,5 +8,8 @@ import com.shinhan.assetManager.apt.MultikeyForAptRecent;
 
 @Repository 
 public interface AptRecentTradeRepo extends CrudRepository<AptRecentTradeDTO, MultikeyForAptRecent>{
+	
+	// aptName, dong, netLeasableArea 3가지(DTO)를 통해 최근거래내역 tradeNo 찾기
+	public AptRecentTradeDTO findByAptNameAndDongAndNetLeasableArea(String aptName, String dong, String netLeasableArea);
 
 }
