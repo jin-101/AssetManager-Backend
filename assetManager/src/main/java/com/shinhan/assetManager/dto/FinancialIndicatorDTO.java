bd9050bd02b1b5c5301902d24342ead515a652c5
djdjdddd <djdj2297@gmail.com>
2023-06-25 03:07:32 +0900

    docs: 차트 라이브러리 사용하기 위해 지표 타입변경하였음 (String => Double)

--- assetManager/src/main/java/com/shinhan/assetManager/dto/FinancialIndicatorDTO.java
+++ assetManager/src/main/java/com/shinhan/assetManager/dto/FinancialIndicatorDTO.java
@@ -20,13 +20,13 @@ public class FinancialIndicatorDTO {
 	
 	private String totalAsset; // 총자산
 	
-	private String householdInd; // 가계수지지표
-	private String totalDebtRepaymentInd; // 총부채상환지표
-	private String consumeDebtRepaymentInd; // 소비생활부채상환지표
-	private String mortgageLoanRepaymentInd; // 거주주택마련부채상환지표
-	private String totalDebtBurdenInd; // 총부채부담지표
-	private String mortgageLoanBurdenInd; // 거주주택마련부채부담지표
-	private String fiInvestInd; // 금융투자성향지표
-	private String fiAssetInd; // 금융자산비중지표
+	private Double householdInd; // 가계수지지표 
+	private Double totalDebtRepaymentInd; // 총부채상환지표
+	private Double consumeDebtRepaymentInd; // 소비생활부채상환지표
+	private Double mortgageLoanRepaymentInd; // 거주주택마련부채상환지표
+	private Double totalDebtBurdenInd; // 총부채부담지표
+	private Double mortgageLoanBurdenInd; // 거주주택마련부채부담지표
+	private Double fiInvestInd; // 금융투자성향지표
+	private Double fiAssetInd; // 금융자산비중지표
 
 } 
